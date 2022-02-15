3f8908c11329b1f0e3a3e586cd179c1cfe924119
lalalalyt <lynneli318@gmail.com>
2022-02-14 22:15:21 -0500

    search words horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  let verticalJoin = [];
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      if (i === 0) {
+        verticalJoin[j] = "";
+      }
+      verticalJoin[j] += letters[i][j];
+    }
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
